77643ed18426e060acf01e7301f659dbd22e0e97
j0zeph <notaj@tutanota.de>
2022-10-16 07:29:42 +0300

    Add custom error pages

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +13,16 @@ def index():
 @app.route('/user/<username>')
 def user(username):
     return render_template('user.html', name=username)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
