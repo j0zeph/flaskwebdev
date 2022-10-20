c7b13af37ace600dad8c1fda4d797d735c2a65e6
j0zeph <notaj@tutanota.de>
2022-10-20 14:50:02 +0300

    Add code for form rendering

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
 from dotenv import load_dotenv
+
 from flask import Flask, render_template
-from flask_moment import Moment
+
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 import os
 
 
@@ -14,10 +19,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('secret_key')
 
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+
+    # if the form was submitted
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = '' # reset the form field
+    
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<username>')
@@ -33,6 +45,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# Form classes
+class NameForm(FlaskForm):
+    name = StringField(label='What is your name?', validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
 
 
 if __name__ == '__main__':
